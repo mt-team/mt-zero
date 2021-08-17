45fb0642bd50279f14bb96913557d0987fedb0bd
vcqr <xingyike@126.com>
2021-08-17 08:48:53 +0000

    update src/util/rhttp/http.go. 增加通用调用

--- src/util/rhttp/http.go
+++ src/util/rhttp/http.go
@@ -1,8 +1,15 @@
 package rhttp
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"reflect"
+	"strings"
 	"time"
+
+	"github.com/tal-tech/go-zero/core/logx"
 )
 
 var (
@@ -27,4 +34,58 @@ func NewHttpClient(timeout int) *Client {
 	}
 
 	return client
+}
+
+func (c *Client) FetchWithJson(option *Option, typ reflect.Type) (interface{}, error) {
+	var err error
+
+	var resp *http.Response
+	switch option.Method {
+	case http.MethodPost:
+		resp, err = c.Post(option.Url, option.Type, strings.NewReader(option.Data))
+	case http.MethodGet:
+		resp, err = c.Get(option.Url)
+	case "FORM":
+		resp, err = c.PostForm(option.Url, option.UrlVal)
+	default:
+		req, err := http.NewRequest(option.Method, option.Url, strings.NewReader(option.Data))
+		if err != nil {
+			logx.Errorf("FetchWithJson: http.NewRequest fail, err=%v", err)
+			return nil, err
+		}
+		if len(option.Headers) > 0 {
+			for _, header := range option.Headers {
+				req.Header.Set(header.Key, header.Val)
+			}
+		}
+		resp, err = c.Do(req)
+	}
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	logx.Infof("FetchWithJson: http status, code=%d", resp.StatusCode)
+
+	if resp.StatusCode != http.StatusOK {
+		logx.Errorf("FetchWithJson: http status not ok, code=%d", resp.StatusCode)
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logx.Errorf("FetchWithJson: read fail, err=%v", err)
+		return nil, err
+	}
+
+	var ret = reflect.New(typ)
+	if err := json.Unmarshal(body, ret.Interface()); err != nil {
+		logx.Errorf("FetchWithJson: json.Unmarshal fail, err=%v", err)
+		return nil, err
+	}
+
+	return ret.Interface(), nil
 }
